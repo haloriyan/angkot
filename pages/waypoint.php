e47c931af23e90918f7c85a1e7a08c247e8a8b36
Riyan Satria <halo@riyansatria.tk>
2018-11-25 00:50:05 +0700

    Updatean

--- pages/waypoint.php
+++ pages/waypoint.php
@@ -6,6 +6,12 @@ $tempatOper = $_COOKIE['tempatOper'];
 setcookie('idangkot', $idangkot, time() + 5555, '/');
 setcookie('idangkots', $idangkots, time() + 5555, '/');
 
+if($tempatOper == '') {
+  $bayar = "Rp 10.000";
+}else {
+  $bayar = "Rp 20.000";
+}
+
 if($idangkots != '') {
   $startRute = $waypoint->start($idangkot);
   $endRute = $waypoint->end($idangkots);
@@ -37,28 +43,73 @@ function getTempatOper($place) {
 	<title>waypoint</title>
 	<link href='aset/fw/build/fw.css' rel='stylesheet'>
 	<link href='aset/fw/build/font-awesome.min.css' rel='stylesheet'>
+  <style>
+    #map {
+      position: absolute;
+      top: 0px;left: 0px;bottom: 0px;
+      width: 60%;
+    }
+    .keterangan {
+      position: absolute;
+      top: 0px;left: 60%;right: 0px;
+      color: #555;
+    }
+    .keterangan .wrap { margin: 10%; }
+  </style>
 </head>
 <body>
 
-<div id="map" style="height: 500px;"></div> 
+<div id="map"></div> 
 <input type="hidden" id="startRute" value="<?php echo $startRute; ?>">
 <input type="hidden" id="endRute" value="<?php echo $endRute; ?>">
 
+<div class="keterangan">
+  <div class="wrap">
+    <h2>Keterangan :</h2>
+    <h3>Berangkat dari :</h3>
+    <span id="addrStart"></span><br /><br />
+    <h3>Menuju :</h3>
+    <span id="addrEnd"></span>
+    <h3>Jarak :</h3>
+    <span id="jarakTempuh"></span>
+    <h3>Oper di :</h3>
+    <?php echo $tempatOper; ?>
+    <h2>Bayar :</h2><?php echo $bayar; ?>
+  </div>
+</div>
+
 
 <script src='aset/js/embo.js'></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8NuAKWkdiDlpdNiJ_HA1l2w6oTYNoZVY&callback=initMap&libraries=places"></script>
 <!-- AIzaSyD8NuAKWkdiDlpdNiJ_HA1l2w6oTYNoZVY -->
 <script>
 	function initMap() {
 		let directionsService = new google.maps.DirectionsService;
 		var directionsDisplay = new google.maps.DirectionsRenderer;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 14,
-          center: {lat: -7.270156914276002, lng: 112.72879890040281}
-        });
-        directionsDisplay.setMap(map);
+    var infowindow = new google.maps.InfoWindow;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: {lat: -7.270156914276002, lng: 112.72879890040281}
+    });
+    directionsDisplay.setMap(map);
 
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
 	}
+  function getPlaceName(setLat, setLng) {
+    var geocoder = new google.maps.Geocoder;
+    let latLng = { lat: parseFloat(setLat), lng: parseFloat(setLng) }
+
+    geocoder.geocode({
+      'location': latLng,
+    }, function(results, status) {
+      if(status === 'OK') {
+        let formattedAddr = results[0].formatted_address
+        return formattedAddr
+      }else {
+        // alert('No result foound')
+      }
+    })
+  }
 
   let startRute = $("#startRute").isi()
   let endRute = $("#endRute").isi()
@@ -87,16 +138,14 @@ function getTempatOper($place) {
          	if (status === 'OK') {
             	directionsDisplay.setDirections(response);
             	var route = response.routes[0];
-            	// var summaryPanel = document.getElementById('directions-panel');
-            	summaryPanel.innerHTML = '';
             	// For each route, display summary information.
             	for (var i = 0; i < route.legs.length; i++) {
               		var routeSegment = i + 1;
-		              summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-                  		'</b><br>';
-              		summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-              		summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-              		summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+		              // summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                //   		'</b><br>';
+                  $("#addrStart").tulis(route.legs[i].start_address)
+                  $("#addrEnd").tulis(route.legs[i].end_address)
+                  $("#jarakTempuh").tulis(route.legs[i].distance.text)
             	}
           	} else {
             window.alert('Directions request failed due to ' + status);
@@ -104,7 +153,6 @@ function getTempatOper($place) {
         });
 	}
 </script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8NuAKWkdiDlpdNiJ_HA1l2w6oTYNoZVY&callback=initMap&libraries=places"></script>
 
 </body>
 </html>
